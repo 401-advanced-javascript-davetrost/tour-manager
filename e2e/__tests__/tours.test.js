ce3085d6e543a0ed23bf4e5524de637c8a3c8ea0
Dave Trost <davetrost@gmail.com>
2019-10-01 17:03:11 -0700

    delete a tour

--- e2e/__tests__/tours.test.js
+++ e2e/__tests__/tours.test.js
@@ -99,43 +99,33 @@ describe('api routes for tours', () => {
       });
   });
 
-  // it('deletes an actor by id', () => {
-  //   return postActor(ed).then(actor => {
-  //     return request
-  //       .delete(`/api/actors/${actor._id}`)
-  //       .expect(200)
-  //       .then(({ body }) => {
-  //         expect(body).toMatchInlineSnapshot(
-  //           {
-  //             _id: expect.any(String),
-  //             dob: expect.any(String)
-  //           },
-  //           `
-  //           Object {
-  //             "__v": 0,
-  //             "_id": Any<String>,
-  //             "dob": Any<String>,
-  //             "name": "Edward Norton",
-  //             "pob": "Boston, Massachusetts",
-  //           }
-  //         `
-  //         );
-  //       });
-  //   });
-  // });
-
-  // it('does not delete an actor who is currently in a film', () => {
-  //   return postFilm(ed, house, fightClub).then(film => {
-  //     return request
-  //       .delete(`/api/actors/${film.cast[0].actor}`)
-  //       .expect(400)
-  //       .then(({ body }) => {
-  //         expect(body).toMatchInlineSnapshot(`
-  //           Object {
-  //             "error": "Cannot remove actor",
-  //           }
-  //         `);
-  //       });
-  //   });
-  // });
+  it('deletes a tour by id', () => {
+    return postTour(initialTour).then(tour => {
+      return request
+        .delete(`/api/tours/${tour._id}`)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toMatchInlineSnapshot(
+            {
+              ...matchMongoId,
+              launchDate: expect.any(String)
+            },
+            `
+            Object {
+              "__v": 0,
+              "_id": StringMatching /\\^\\[a-f\\\\d\\]\\{24\\}\\$/i,
+              "activities": Array [
+                "a-capella singing",
+                "freestyle frisbee",
+                "drumming",
+              ],
+              "launchDate": Any<String>,
+              "stops": Array [],
+              "title": "Movement show",
+            }
+          `
+          );
+        });
+    });
+  });
 });
